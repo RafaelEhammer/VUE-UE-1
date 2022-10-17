4a1adc57294673b432c39611c1c1acf9b1ea3c14
UsernameTaken4 <74597434+UsernameTaken4@users.noreply.github.com>
2022-10-17 11:43:50 +0200

    Standpunkt 14.10.2022

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,57 @@
 <template>
   <div id="app">
-    <h1>Guten Tag!</h1>
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="5BHIT" />
+    <h3>Example</h3>
+    <form @submit.prevent="addNew">
+      <label>Enter new subject:</label>
+      <input v-model="newSubject" />
+      <button>Add subject</button>
+      <hr />
+    </form>
+    <ul>
+      <li v-for="(item, index) in subjectArray">
+        {{ item.content }}
+        <button @click="deleteItem(item)">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
+  setup() {
+    const newSubject = ref('');
+    const subjectArray = ref([]);
+    let id = 0;
+
+    function addNew() {
+      console.log('addNew...');
+      subjectArray.value.push({
+        content: newSubject.value,
+        iid: id,
+      });
+      console.log(subjectArray.value);
+      id++;
+    }
+
+    function deleteItem(item) {
+      console.log(subjectArray.value[0].content);
+      for (let i = 0; i < subjectArray.value.length; i++) {
+        if (subjectArray.value[i].iid === item.iid) {
+          subjectArray.value.splice(i, 1);
+          return;
+        }
+      }
+    }
+
+    return {
+      newSubject,
+      subjectArray,
+      addNew,
+      deleteItem,
+    };
   },
 };
 </script>
